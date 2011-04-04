0cb69e57f0b47ca26e0c0a0c092d25156fb8592c
Wout Slakhorst <wout.slakhorst@nedap.com>
2011-04-04 11:43:58 +0200

    Validation!

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,9 @@
 class Product < ActiveRecord::Base
+  validates :price, :numericality => {:greater_than_or_equal_to => 0.01}, :presence => true
+  validates :title, :uniqueness => true, :presence => true
+  validates :description, :presence => true
+  validates :image_url, :presence => true, :format => {
+    :with     => %r{\.(gif|jpg|png)$}i,
+    :message  => 'must be a URL for GIF, JPG, or PNG image.'
+  }
 end
