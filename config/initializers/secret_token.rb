ce75af9d533c5418b8e281b95be3bed00028c22a
Wout Slakhorst <wout.slakhorst@nedap.com>
2011-04-04 09:53:38 +0200

    default rails structure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = 'ebc3c2f3c1103f4ec9309f6f01b42f67f8cd4bf6b30d2f16329ac72a7a102cc5146091bcc15458050d260fc54bf4bb7483e23e414b592a92d22c2ad19536d7b4'
